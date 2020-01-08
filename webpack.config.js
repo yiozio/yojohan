dd1c7727137a5d446c566f0bb2324a03342255f9
Yuzo <yoozoo.mng@gmail.com>
2020-01-09 02:35:38 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const DEVELOPMENT = 'development';
+
+module.exports = (env, argv) => ({
+    mode: argv.mode ? argv.mode : DEVELOPMENT,
+    entry: {yojohan: './src/index.tsx'},
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: ['ts-loader']
+        }]
+    },
+    devtool: !argv.mode || argv.mode === DEVELOPMENT ? 'source-map' : undefined,
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        path: path.resolve('./public/dist'),
+        filename: '[name].js'
+    },
+    stats: {
+        children: false
+    }
+})
